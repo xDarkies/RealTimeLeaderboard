becbdf093515db0a6c34ef767cb493a0872ad110
Madness <awronski16@gmail.com>
2026-07-02 16:50:27 +0200

    feat: add useSocket composable

---
+++ client/src/composables/useSocket.ts
@@ -0,0 +1,36 @@
+import { ref, onMounted, onUnmounted} from "vue"
+import {io, type Socket}  from "socket.io-client"
+
+type Leaderboard = {
+    rank: number,
+    username: string,
+    score: number
+}
+
+let socket: Socket | null = null
+const leaderboard = ref<Leaderboard[]>([])
+const isConnected = ref(false)
+
+export function useSocket() {
+    if(!socket){
+        socket = io("ws://localhost:3000")
+
+        socket.on("connect", () => {
+            isConnected.value = true;
+        })
+
+        socket.on("disconnect", () => {
+            isConnected.value = false;
+        })
+
+        socket.on("leaderboard", (data: Leaderboard[]) => {
+            leaderboard.value = data;
+        })
+    }
+
+    return {
+        socket,
+        leaderboard,
+        isConnected
+    }
+}
